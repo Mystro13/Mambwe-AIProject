2442ce5edf1b2fbada98e1672c7a3173b994da3e
Mambwe Mundemba <mambwe.mundemba@outlook.com>
2022-01-31 22:00:04 -0600

    Initial commit

---
+++ scriptingProject/Assets/Scripts/Interactables/InteractableSystemScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSystemScript : MonoBehaviour
+{
+
+    private IInteractable currentInteractable = null;
+    
+
+
+    private void Update()
+    {
+        CheckForInteraction();
+    }
+    private void CheckForInteraction()
+    {
+
+        if (currentInteractable == null) { return; }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentInteractable.Interact();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var interactable = other.GetComponent<IInteractable>();
+
+        if(interactable == null) { return; }
+
+        currentInteractable = interactable;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        var interactable = other.GetComponent<IInteractable>();
+
+        if (interactable == null) { return; }
+
+        if(interactable != currentInteractable) { return; }
+
+        currentInteractable = null;
+    }
+} 
